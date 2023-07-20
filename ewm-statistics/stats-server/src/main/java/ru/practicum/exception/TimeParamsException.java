b3cf6b978329fddec260fff375197e7c16fb20e5
Katty <katty.1996@mail.ru>
2023-07-21 01:17:25 +0300

    chekstyle

--- ewm-statistics/stats-server/src/main/java/ru/practicum/exception/TimeParamsException.java
+++ ewm-statistics/stats-server/src/main/java/ru/practicum/exception/TimeParamsException.java
@@ -1,6 +1,6 @@
 package ru.practicum.exception;
 
-public class TimeParamsException extends RuntimeException{
+public class TimeParamsException extends RuntimeException {
     public TimeParamsException(String message) {
         super(message);
     }
